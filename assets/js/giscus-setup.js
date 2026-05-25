7bf9fba2d2a7e2b6a37e956c0eb2346d5c8224ea
tjleizeng <tjleizeng@users.noreply.github.com>
2026-05-25 21:07:34 +0000

    Deploying to gh-pages from @ tjleizeng/tjleizeng.github.io@70c1aad6dc2a7b50288e84f007b9735b9ffc88ae 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tjleizeng/-tjleizeng.github.io",
+    "data-repo": "tjleizeng/tjleizeng.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
